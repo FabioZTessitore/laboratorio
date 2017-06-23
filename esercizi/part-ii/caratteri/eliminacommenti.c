ea82d7c88f5aac4b1de80df34a97e450d4152bd9
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-06-23 12:01:32 +0200

    fix eliminacommenti.c

--- esercizi/part-ii/caratteri/eliminacommenti.c
+++ esercizi/part-ii/caratteri/eliminacommenti.c
@@ -2,8 +2,124 @@
 
 /* elimina i commenti da un file C */
 
-/* Nel codice sono presenti vari commenti a scopo di test
- * cosi' da usare il codice stesso per i test */
+/* Sono presenti vari commenti cosi' da usare il codice stesso per i test */
+
+/* I commenti in C iniziano quando si incontra il carattere '/' seguito da '*',
+quindi per determinare se e' iniziato un commento non basta l'arrivo di uno '/'.
+
+Per iniziare, definiamo i seguenti stati:
+
+1) C        codice C
+2) SLASH    incontrato uno '/' all'interno del codice
+3) COMM     incontrato un '*' dopo uno '/', inizia il commento
+
+Allo stesso modo, per determinare la fine di un commento dobbiamo incontrare
+un '*' seguito da uno '/'. Aggiungiamo allora lo stato:
+
+4) AST      incontrato un '*' all'interno di un commento
+            (se il prossimo carattere e' uno '/' si torna nello stato C)
+
+Una prima tabella degli stati potrebbe essere:
+
+Stato       Input     Output      Nuovo Stato
+    C       c=='/'       -          SLASH
+    C       c!='/'       c            -
+
+SLASH       c=='*'       -          COMM
+SLASH       c!='*'   '/' + c          C
+
+ COMM       c=='*'       -           AST
+ COMM       c!='*'       -            -
+
+  AST       c=='/'       -            C
+  AST       c!='/'       -          COMM
+
+Che succede se un commento viene inserito all'interno di una stringa?
+In base alla tabella degli stati che abbiamo creato finora il commento
+verrebbe eliminato, ma sarebbe un errore. Aggiungiamo allora un nuovo stato:
+
+5) STR      incontrate virgolette doppie all'interno del codice, inizia una stringa
+
+Determinare quando inizia una stringa a questo punto e' banale. Un problema
+piu' grande si ha quando vogliamo determinare la fine di una stringa
+perche' all'interno di essa potrebbe essere presente la sequenza \".
+In questo caso le virgolette non significano la chiusura della stringa!
+Aggiungiamo (temporaneamente) un altro stato:
+
+6*) INVSLASHSTR   incontrato '\' all'interno di una stringa
+
+La tabella degli stati si modifica:
+
+Stato       Input     Output      Nuovo Stato
+      C       c=='"'       c           STR            * inizia una stringa *
+      C       c=='/'       -          SLASH
+      C       c!='/'       c            -
+
+    STR       c=='\'       c         INVSLASH         * trovato '\' *
+    STR       c=='"'       c            C             * chiusura stringa *
+    STR       else         c            -
+
+INVSLASHSTR  qualunque     c           STR
+
+  SLASH       c=='*'       -          COMM
+  SLASH       c!='*'   '/' + c          C
+
+   COMM       c=='*'       -           AST
+   COMM       c!='*'       -            -
+
+    AST       c=='/'       -            C
+    AST       c!='/'       -          COMM
+
+In realta' quando siamo nello stato INVSLASHSTR dobbiamo sempre stampare
+il carattere che segue e poi tornare allo stato STR. Quindi possiamo eliminare
+questo stato andando a modificare il comportamento del programma quando
+incontra lo '\'.
+
+Stato       Input     Output      Nuovo Stato
+      C       c=='"'       c           STR            * inizia una stringa *
+      C       c=='/'       -          SLASH
+      C       else         c            -
+
+    STR       c=='\'       c            -             * trovato '\', legge e stampa il carattere successivo, resta in STR *
+    STR       c=='"'       c            C             * chiusura stringa *
+    STR       else         c            -
+
+  SLASH       c=='*'       -          COMM
+  SLASH       c!='*'   '/' + c          C
+
+   COMM       c=='*'       -           AST
+   COMM       c!='*'       -            -
+
+    AST       c=='/'       -            C
+    AST       c!='/'       -          COMM
+
+Ci siamo quasi, bisogna considerare ancora il caso dei singoli apici.
+La gestione sara' simile a quella delle stringhe.
+
+6) APICE         incontrato apice
+
+Stato       Input     Output      Nuovo Stato
+      C    c=='"'       c           STR            * inizia una stringa *
+      C    c=='''       c          APICE           * un carattere *
+      C    c=='/'       -          SLASH
+      C    else         c            -
+
+    STR    c=='\'       c            -             * trovato '\', legge e stampa il carattere successivo, resta in STR *
+    STR    c=='"'       c            C             * chiusura stringa *
+    STR    else         c            -
+
+  APICE    c=='\'       c            -             * trovato '\', legge e stampa il carattere successivo, resta in APICE *
+  APICE    c=='''       c            C             * chiusura apici *
+
+  SLASH    c=='*'       -          COMM
+  SLASH    c!='*'   '/' + c          C
+
+   COMM    c=='*'       -           AST
+   COMM    c!='*'       -            -
+
+    AST    c=='/'       -            C
+    AST    c!='/'       -          COMM
+*/
 
 #include <stdio.h>
 
@@ -14,8 +130,7 @@ int main()
                 AST,    /* INCONTRATO '*' IN COMMENTO */
                 COMM,   /* COMMENTO */
                 STR,    /* STRINGA */
-                APICE,  /* INCONTRATO apice in CODICE */
-                INVSLASH,
+                APICE   /* APICI */
   };
   int stato = C;
   int c;
@@ -27,36 +142,52 @@ int main()
   "Questa stringa contiene un commento\n"
   "/* non sono un vero commento ... */\n"
   "solo a scopo dimostrativo.\n\n"
-  "E anche un carattere 'c'!!!";
+  "E anche un carattere 'c'!!!"
+  "E una \"stringa\"";
 
   while ( (c = getchar()) != EOF ) {
     /* nello stato C controlla se incontra:
-     '/'  che potrebbe significare inizio di un commento;
-     '\"' che significa inizio di una stringa
+    '\"' significa inizio di una stringa
+    '\'' significa inizio di apici
+    '/'  potrebbe significare inizio di un commento
     */
-    if (stato==C) {
-      if (c == '/')
-        stato = SLASH;
-      else if (c == '\"') {
+    if (stato == C) {
+      if (c == '\"') {
         putchar(c);
         stato = STR;
       } else if (c == '\'') {
         putchar(c);
         stato = APICE;
-      } else
+      } else if (c == '/') {
+        stato = SLASH;
+      } else {
         putchar(c);
+      }
     } else if (stato == STR) {
       /* all'interno di una "stringa" bisogna sempre
       stampare fino al carattere \" di chiusura stringa */
       putchar(c);
-      if (c == '\"') stato = C;
+      if (c == '\\') {
+        c = getchar();
+        putchar(c);
+      } else if (c == '\"') {
+        stato = C;
+      }
+    } else if (stato == APICE) {
+        putchar(c);
+        if (c == '\\') {
+          c = getchar();
+          putchar(c);
+        } else if (c == '\'') {
+          stato = C;
+        }
     } else if (stato == SLASH) {
       /* nello stato SLASH controlla se incontra '*'
       nel qual caso inizia un commento */
       if (c != '*') {
-        stato = C;
         putchar('/');
         putchar(c);
+        stato = C;
       } else {
         stato = COMM;
       }
@@ -68,21 +199,11 @@ int main()
     } else if (stato == AST) {
       /* nello stato AST controlla se arriva il carattere '/'
       che determina la fine del commento */
-      if (c != '/') stato = COMM;
-      else stato = C;
-    } else if (stato == APICE) {
-      if (c == '\\') {
-        putchar(c);
-        stato = INVSLASH;
+      if (c != '/') {
+        stato = COMM;
       } else {
-        putchar(c);
-        if (c == '\'') {
-          stato = C;
-        }
+        stato = C;
       }
-    } else if (stato = INVSLASH) {
-      putchar(c);
-      stato = APICE;
     }
   }
 
