890b267a6c2bd01494b616d98222eaeb2eed8ce3
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-12-05 14:26:27 +0100

    typos

--- esercizi/part-iii/myvect/confronto/myvect_sort.h
+++ esercizi/part-iii/myvect/confronto/myvect_sort.h
@@ -1,7 +1,7 @@
 /* myvect_sort.h */
 
-#ifndef MYVECT_SORT
-#define MYVECT_SORT
+#ifndef MYVECT_SORT_H
+#define MYVECT_SORT_H
 
 void myvect_bubble_sort(int v[], const int size);
 void myvect_selection_sort(int v[], const int size);
