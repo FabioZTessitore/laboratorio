a49e5b3535fef1db0c6a6540997eefda4584d616
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-12-24 18:54:45 +0100

    stampa stringhe

---
+++ esercizi/part-iii/puntvett/stampa_stringhe.c
@@ -0,0 +1,22 @@
+/* stampa_stringhe.c */
+
+/* Stampa le stringhe contenute in un vettore di stringhe */
+
+/* Istruzioni per la compilazione e l'esecuzione:
+$ make stampa_stringhe
+$ ./stampa_stringhe
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  const char * const semi[] = { "Cuori", "Quadri", "Fiori", "Picche" };
+  int i;
+
+  for (i = 0; i < 4; i++) {
+    puts(semi[i]);
+  }
+
+  return 0;
+}
