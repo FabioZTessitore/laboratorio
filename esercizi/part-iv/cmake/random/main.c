dd3cbd15d06f9ed6f3a5fb76d3cd7f7a2e94770e
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-02-08 15:07:54 +0100

    cmake

---
+++ esercizi/part-iv/cmake/random/main.c
@@ -0,0 +1,24 @@
+/* main.c */
+
+/* Uso di una libreria */
+
+/* istruzione per la compilazione e l'esecuzione:
+ *
+ * $ mkdir build
+ * $ cd build
+ * $ cmake ..
+ * $ make
+ * $ ./Number
+ */
+
+#include <stdio.h>
+#include "random.h"
+
+int main()
+{
+  random_init();
+
+  printf("Una cifra casuale: %d\n", random_between(0, 10));
+
+  return 0;
+}
