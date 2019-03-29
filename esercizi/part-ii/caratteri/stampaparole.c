94dd0b547db65415a276578d9fbdd8158117cd5f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-03-29 13:06:32 +0100

    aggiunto stato START

--- esercizi/part-ii/caratteri/stampaparole.c
+++ esercizi/part-ii/caratteri/stampaparole.c
@@ -10,11 +10,22 @@ $ ./stampaparole < testo.txt
 
 /*
 Stati possibili:
-  1)  IN       all'interno di una parola
-  2)  OUT      all'esterno delle parole
+  1)  START    stato iniziale, utilizzato per
+                capire se il primo carattere
+                e' di spaziatura o meno
+  2)  IN       all'interno di una parola
+  3)  OUT      all'esterno delle parole
 
 Tabella degli stati:
 Stato       Input          Output      Nuovo Stato
+START     c==' '  or
+          c=='\n' or
+          c=='\t'            -            OUT
+
+START     c!=' '  and
+          c!='\n' and
+          c!='\t'            c             IN
+
  IN       c==' '  or
           c=='\n' or
           c=='\t'           '\n'           OUT
@@ -30,13 +41,20 @@ Stato       Input          Output      Nuovo Stato
 
 int main()
 {
-  enum Stato { IN, OUT };
-  int stato = IN;
+  enum Stato { START, IN, OUT };
+  int stato = START;
 
   int c;
 
   while ( (c=getchar()) != EOF ) {
-    if (stato == IN) {
+    if (stato == START) {
+      if (c==' ' || c=='\n' || c=='\t') {
+        stato = OUT;
+      } else {
+        putchar(c);
+        stato = IN;
+      }
+    } else if (stato == IN) {
       if (c==' ' || c=='\n' || c=='\t') {
         putchar('\n');
         stato = OUT;
