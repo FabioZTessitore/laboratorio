b2f2e32eff2e6f884c47de43c32dcaf26f2f5cdc
Fabio Z Tessitore <fabioztessitore@gmail.com>
2020-04-25 22:11:12 +0200

    notazione polacca inversa

---
+++ esercizi/part-iv/compilatore/notazione_polacca_inversa/main.c
@@ -0,0 +1,203 @@
+/* main.c */
+
+/* Conversione di una espressione scritta in notazione infissa in notazione polacca inversa */
+
+/* Es.
+ *
+ * 8 + 3 ---> 8 3 +
+ *
+ * (8 + 3) * 5 ---> 8 3 + 5 *
+ * (24 + 4) * 8 ---> 24 4 + 8 *
+ */
+
+/* Operazione preliminare:
+ *
+ * Siccome i numeri possono essere composti da piu' cifre, la prima operazione
+ * e' la creazione di una coda contenente i vari token (parentesi, operatori, numeri)
+ *
+ * Dati:
+ * - infix      il vettore contenente l'espressione da convertire
+ * - token      coda contenente la lista dei token
+ *
+ * 1) Per ogni carattere di infix
+ * 2)     se e' spazio ignora
+ * 3)     se e' una parentesi o un operatore inserisce in coda
+ * 4)     se e' una cifra, determina la fine delle cifre e inserisce in coda
+ *
+ * Es.
+ *
+ * 8 + 3 ---> 8 --> + --> 3 --> NULL
+ * (8 + 3) * 5 ---> ( --> 8 --> + --> 3 --> ) --> * --> 5 --> NULL
+ * (24 + 4) * 8 ---> ( --> 24 --> + --> 4 --> ) --> * --> 8 --> NULL
+ */
+ 
+/*
+ * Algoritmo di conversione:
+ *
+ * Dati:
+ * - token      la coda contenente i token
+ * - postfix    il vettore contenente l'espressione convertita
+ * - stack      uno stack per la memorizzazione temporanea dei caratteri
+ *
+ *  1) inserire una parentesi aperta nello stack e una chiusa al termine di token
+ *  2) finche' lo stack non e' vuoto:
+ *  3)     estrae il primo token
+ *  4)     se e' un numero lo copia in postfix
+ *  5)     se e' una parentesi aperta la inserisce nello stack
+ *  6)     se e' una parentesi chiusa:
+ *  7)         estrae gli operatori dallo stack fino a che non trova una parentesi aperta e li inserisce in postfix
+ *  8)         elimina la parentesi aperta dallo stack
+ *  9)     se e' un operatore:
+ * 10)         estrae gli operatori di priorita' maggiore o uguale a quello corrente dallo stack e li inserisce in postfix
+ * 11)         inserisce nello stack l'operatore corrente
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "queue.h"
+#include "stack.h"
+
+int priority(char *op, char *t);
+
+int main(void)
+{
+    Queue token;
+    char infix[] = "(2 * (8 + 3)) + 5 * 43 + 1";
+    char *current;
+    char buffer[80];
+    int k;
+    
+    Stack s;
+    char *stack_item;
+    char postfix[80] = "";
+
+    queue_init(&token);
+    stack_init(&s);
+
+    /* Operazione preliminare: creazione della coda di token */
+    current = infix;
+    while (*current) {
+        if (isspace(*current)) {
+            /* ignora spazi */
+        } else if (!isdigit(*current)) {
+            /* parentesi ed operatori */
+            buffer[0] = *current;
+            buffer[1] = '\0';
+            queue_enqueue(&token, buffer);
+        } else {
+            /* numeri */
+            k = 0;
+            buffer[k] = *current;
+            current++;
+            while (isdigit(*current)) {
+                buffer[++k] = *current;
+                current++;
+            }
+            buffer[++k] = '\0';
+            queue_enqueue(&token, buffer);
+            continue;
+        }
+        current++;
+    }
+
+    printf("Token:\n");
+    queue_print(&token);
+    putchar('\n');
+
+    /* Algoritmo */
+
+    /* 1) inserire una parentesi aperta nello stack e una chiusa al termine di token */
+    stack_push(&s, "(");
+    queue_enqueue(&token, ")");
+
+    printf("Dopo il passo 1:\n");
+    printf("Token:\n");
+    queue_print(&token);
+    printf("Stack:\n");
+    stack_print(&s);
+    putchar('\n');
+
+    /* 2) finche' lo stack non e' vuoto: */
+    while (!stack_isEmpty(&s)) {
+        /* 3) estrae il primo token */
+        current = queue_dequeue(&token);
+
+        /* 4) se e' un numero lo copia in postfix */
+        if (isdigit(*current)) {
+            strcat(postfix, current);
+            strcat(postfix, " ");
+        }
+        
+        /* 5) se e' una parentesi aperta la inserisce nello stack */
+        else if (*current == '(') {
+            stack_push(&s, current);
+        }
+        
+        /* 6) se e' una parentesi chiusa: */
+        else if (*current == ')') {
+            /* 7) estrae gli operatori dallo stack fino a che non trova una
+                  parentesi aperta e li inserisce in postfix */
+            while (strcmp(stack_top(&s), "(") != 0) {
+                stack_item = stack_pop(&s);
+                strcat(postfix, stack_item);
+                strcat(postfix, " ");
+                free(stack_item);
+            }
+            /* 8) elimina la parentesi aperta dallo stack */
+            stack_item = stack_pop(&s);
+            free(stack_item);
+        }
+
+        /* 9) se e' un operatore: */
+        else {
+            /* 10) estrae gli operatori di priorita' maggiore o uguale
+                   a quello corrente dallo stack e li inserisce in postfix */
+            while (priority(stack_top(&s), current)) {
+                stack_item = stack_pop(&s);
+                strcat(postfix, stack_item);
+                strcat(postfix, " ");
+                free(stack_item);
+            }
+            /* 11) inserisce nello stack l'operatore corrente */
+            stack_push(&s, current);
+        }
+
+        free(current);
+    }
+
+    queue_clear(&token);
+    stack_clear(&s);
+
+    printf("POSTFIX: %s\n", postfix);
+
+    return 0;
+}
+
+int priority(char *op, char *t)
+{
+    if (strcmp(op, "*") != 0 &&
+        strcmp(op, "/") != 0 &&
+        strcmp(op, "+") != 0 &&
+        strcmp(op, "-") != 0) {
+            return 0;
+    }
+    
+    if (strcmp(op, "*") == 0 ||
+        strcmp(op, "/") == 0) {
+            return 1;
+    }
+    
+    if (strcmp(op, "+") == 0 ||
+        strcmp(op, "-") == 0) {
+            if (strcmp(t, "*") == 0 ||
+                strcmp(t, "/") == 0) {
+                    return 0;
+            } else {
+                return 1;
+            }
+    }
+
+    return 0;
+}
---
+++ esercizi/part-iv/compilatore/notazione_polacca_inversa/queue.c
@@ -0,0 +1,99 @@
+/* queue.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "queue.h"
+#include "node.h"
+
+void queue_init(Queue * const q)
+{
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+}
+
+int queue_isEmpty(const Queue * const q)
+{
+    return q->size == 0;
+}
+
+int queue_size(const Queue * const q)
+{
+    return q->size;
+}
+
+void queue_print(const Queue * const q)
+{
+    Node *current = q->head;
+
+    while (current != NULL) {
+        node_print(current);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void queue_enqueue(Queue * const q, char *value)
+{
+    Node *newNode = (Node *) malloc( sizeof(Node) );
+    if (!newNode) {
+        fprintf(stderr, "Impossibile allocare memoria\n");
+        return;
+    }
+    node_init(newNode, value);
+    newNode->next = NULL;
+
+    q->size++;
+
+    if (q->head == NULL) {
+        q->head = newNode;
+    } else {
+        q->tail->next = newNode;
+    }
+
+    q->tail = newNode;
+}
+
+char *queue_dequeue(Queue * const q)
+{
+    Node *temp;
+    char *value;
+
+    value = (char *) malloc(strlen(q->head->s)+1);
+    strcpy(value, q->head->s);
+
+    temp = q->head;
+    q->head = q->head->next;
+
+    if (q->head == NULL) {
+        q->tail = NULL;
+    }
+
+    printf("Libero memoria per l'elemento %s\n", temp->s);
+    node_free(temp);
+    free(temp);
+
+    q->size--;
+
+    return value;
+}
+
+void queue_clear(Queue * const q)
+{
+    Node *current = q->head;
+    Node *temp;
+
+    while (current != NULL) {
+        temp = current;
+        current = current->next;
+
+        printf("Libero memoria per l'elemento %s\n", temp->s);
+        node_free(temp);
+        free(temp);
+    }
+
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+}
