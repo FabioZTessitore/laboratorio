7a561e7b301468000dd3bf79a9130d6cbec4a695
Fabio Z Tessitore <fabioztessitore@gmail.com>
2022-03-15 19:03:29 +0100

    .

--- esercizi/introduzione_alla_programmazione/primogrado.py
+++ esercizi/introduzione_alla_programmazione/primogrado.py
@@ -1,22 +1,22 @@
 # primogrado.py
 
 # risolve equazioni di primo grado
-# nella forma Ax + B = 0, con A e B interi
+# nella forma ax + b = 0, con a e b interi
 
 print("Risolve equazioni di primo grado")
-print("nella forma Ax + B = 0\n")
+print("nella forma ax + b = 0\n")
 
-A = input('A: ')
-A = int(A)
-B = input('B: ')
-B = int(B)
+a = input('a: ')
+a = int(a)
+b = input('b: ')
+b = int(b)
 
-if A==0 and B==0:
+if a==0 and b==0:
     print("L'equazione 0 = 0 ammette infinite soluzioni")
 
-elif A==0:
-    print("L'equazione", B, "= 0 non ammette soluzioni")
+elif a==0:
+    print("L'equazione", b, "= 0 non ammette soluzioni")
 
 else:
-    x = -B / A
+    x = -b / a
     print("L'equazione ammette soluzione:", x)
